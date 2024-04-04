639cad8417c4eac3fc4323f0f5ffab5882d85a47
devayesha23 <zahraayesha414@gmail.com>
2024-04-05 02:46:01 +0500

    my commit

--- api/config.php
+++ api/config.php
@@ -1,5 +1,5 @@
 <?php
 // Aviationstack API Configuration
-$api_key = "cbb0c533361bfe3371e04947ec7568b6"; // Replace with your Aviationstack API key
+$api_key = "9b2b11bd0d847df1ae627c62a9a27800"; // Replace with your Aviationstack API key
 $base_url = "http://api.aviationstack.com/v1/";
 ?>
